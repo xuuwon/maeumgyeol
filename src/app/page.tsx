dcd0b544425fee6720c570554f8b02cb456cf1cf
xuuwon <dumpling1011@gmail.com>
2025-05-19 00:36:39 +0900

    feat: 임시 랜딩 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
+import Button from '@/components/button/Button';
+import Image from 'next/image';
+
 export default function Home() {
-  return <div className="text-[#452]">마음결</div>;
+  return (
+    <div className="flex flex-col items-center justify-around w-full h-screen py-12">
+      {/* 제목 */}
+      <div className="text-3xl sm:text-4xl">마음결 (心結)</div>
+
+      {/* 이미지 */}
+      <div>
+        <Image
+          src="/images/landing.png" // public 폴더 기준 경로 또는 외부 URL
+          alt="설명 텍스트"
+          width={300} // 이미지 가로 크기(px)
+          height={300} // 이미지 세로 크기(px)
+        />
+      </div>
+
+      {/* 서비스 소개 */}
+      <div className="w-64 max-w-full sm:w-full">
+        <p className="text-center">
+          마음결은 당신의 감정을 이해하고 치유하는 맞춤형 심리 상담 플랫폼입니다.
+          <br /> 언제 어디서나 편안하게, 전문가와 함께 마음 건강을 케어하세요.
+        </p>
+      </div>
+
+      {/* 버튼 */}
+      <div className="flex flex-col items-center w-full gap-2">
+        <Button type="yellow" text="로그인" />
+        <Button type="white" text="회원가입" />
+      </div>
+    </div>
+  );
 }
