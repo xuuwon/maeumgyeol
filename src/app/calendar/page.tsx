6213b63ebff10136b77244001b5ecbe1cbb7bf42
xuuwon <dumpling1011@gmail.com>
2025-05-20 11:37:06 +0900

    feat: 폴더, 파일 추가

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => {
+  return <div>캘린더</div>;
+};
+
+export default Page;
---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => {
+  return <div>홈</div>;
+};
+
+export default Page;
