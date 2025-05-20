6101b34c702cd13e9c1e0ae9f8b08e3268dd176d
xuuwon <dumpling1011@gmail.com>
2025-05-20 11:36:22 +0900

    feat: navBar 컴포넌트 구현

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { CalendarDays, ChartColumn, House, User } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const NavBar = () => {
+  const router = useRouter();
+
+  const iconStyle = 'text-[#A5A5A5] hover:text-[#636363] cursor-pointer';
+
+  return (
+    <div className="absolute bottom-0 left-0 flex items-center justify-between w-full h-16 px-4 sm:px-6 md:px-8">
+      <House className={iconStyle} onClick={() => router.push('/home')} />
+      <CalendarDays className={iconStyle} onClick={() => router.push('/calendar')} />
+      <ChartColumn className={iconStyle} onClick={() => router.push('/statistic')} />
+      <User className={iconStyle} onClick={() => router.push('/profile')} />
+    </div>
+  );
+};
+
+export default NavBar;
