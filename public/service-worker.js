5f3d6334764c832ac026548abd9fa603e18d3fa3
xuuwon <dumpling1011@gmail.com>
2025-05-28 00:56:46 +0900

    feat: PWA 관련 설정 변경

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'my-pwa-cache-v1';
-const urlsToCache = ['/', '/index.html'];
+const urlsToCache = ['/'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
