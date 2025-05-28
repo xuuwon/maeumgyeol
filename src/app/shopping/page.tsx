be65be453102ff7598d9c23d152d9d0b5203c737
xuuwon <dumpling1011@gmail.com>
2025-05-28 16:15:43 +0900

    feat: 임시 유저 데이터 추가, 보유 중 아이템 구분 추가

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,93 +1,133 @@
 'use client';
 
+import clsx from 'clsx';
 import Image from 'next/image';
 import React, { useState } from 'react';
 
 const Page = () => {
   const [selectedCategory, setSelectedCategory] = useState<'배경' | '액세서리'>('배경');
 
+  const userData = {
+    userId: 'user1234',
+    nickname: '감자몽러버',
+    gender: 'female',
+    birthday: '2000-05-01',
+    character: '/images/characters/basic_character.png',
+    coin: 1500,
+  };
+
   const data = [
+    // 임시 데이터
     {
+      id: 1,
       name: '수박바',
-      description: '여름엔 시원한 수박바!',
+      category: '액세서리',
       price: 200,
       image: '/images/items/watermelon_item.png',
       appliedImage: '/images/characters/watermelon_character.png',
-      category: '액세서리',
+      description: '여름엔 시원한 수박바!',
+      isOwned: true,
+      isEquipped: false,
     },
     {
+      id: 2,
       name: '랜덤 박스',
-      description: '감자몽이 어떻게 바뀔까요?',
+      category: '액세서리',
       price: 500,
       image: '/images/items/sweetpotato_item.png',
       appliedImage: '/images/characters/sweetpotato_character.png',
-      category: '액세서리',
+      description: '감자몽이 어떻게 바뀔까요?',
+      isOwned: false,
+      isEquipped: false,
     },
     {
+      id: 3,
       name: '선글라스',
-      description: '햇빛이 쨍쨍할 땐 선글라스 필수!',
+      category: '액세서리',
       price: 200,
       image: '/images/items/sunglasses_item.png',
       appliedImage: '/images/characters/sunglasses_character.png',
-      category: '액세서리',
+      description: '햇빛이 쨍쨍할 땐 선글라스 필수!',
+      isOwned: true,
+      isEquipped: false,
     },
     {
+      id: 4,
       name: '리본',
-      description: '감자몽을 공주로 만들어 볼까요?',
+      category: '액세서리',
       price: 300,
       image: '/images/items/ribbon_item.png',
       appliedImage: '/images/characters/ribbon_character.png',
-      category: '액세서리',
+      description: '감자몽을 공주로 만들어 볼까요?',
+      isOwned: false,
+      isEquipped: false,
     },
     {
+      id: 5,
       name: '흙더미',
-      description: '흙더미에 감자몽을 빠뜨려 보아요!',
+      category: '액세서리',
       price: 500,
       image: '/images/items/soil_item.png',
       appliedImage: '/images/characters/soil_character.png',
-      category: '액세서리',
+      description: '흙더미에 감자몽을 빠뜨려 보아요!',
+      isOwned: false,
+      isEquipped: false,
     },
     {
+      id: 6,
       name: '고양이',
-      description: '감자몽은 고양이를 좋아해요.',
+      category: '액세서리',
       price: 400,
       image: '/images/items/cat_item.png',
       appliedImage: '/images/characters/cat_character.png',
-      category: '액세서리',
+      description: '감자몽은 고양이를 좋아해요.',
+      isOwned: false,
+      isEquipped: false,
     },
     {
+      id: 7,
       name: '토마토',
-      description: '상큼한 토마토 좋아하세요?',
+      category: '액세서리',
       price: 300,
       image: '/images/items/tomato_item.png',
       appliedImage: '/images/characters/tomato_character.png',
-      category: '액세서리',
+      description: '상큼한 토마토 좋아하세요?',
+      isOwned: false,
+      isEquipped: false,
     },
     {
+      id: 8,
       name: '벚꽃 배경',
-      description: '벚꽃 흩날리는 언덕에서 휴식을 취해봐요!',
+      category: '배경',
       price: 500,
       image: '/images/background/cherry-blossom.jpg',
       appliedImage: '/images/background/cherryBlossom_tablet.png',
-      category: '배경',
+      description: '벚꽃 흩날리는 언덕에서 휴식을 취해봐요!',
+      isOwned: true,
+      isEquipped: false,
     },
     {
+      id: 9,
       name: '해변 배경',
-      description: '시원한 해변에서 물놀이 한 번 어때요?',
+      category: '배경',
       price: 500,
       image: '/images/background/beach.jpg',
       appliedImage: '/images/background/beach_mobile.png',
-      category: '배경',
+      description: '시원한 해변에서 물놀이 한 번 어때요?',
+      isOwned: false,
+      isEquipped: false,
     },
   ];
 
   const filteredItems = data.filter((item) => item.category === selectedCategory);
-  const [settingCharacter, setSettingCharacter] = useState<string>(
-    '/images/characters/basic_character.png'
-  ); // 유저 정보의 캐릭터
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+  const [buyItem, setBuyItem] = useState<string>(userData.character);
+  // 미리보기 후 구매용 - 초기값은 유저 정보의 캐릭터
+  const [buyBackground, setBuyBackground] = useState<string>('');
+  const [showBuyButton, setShowBuyButton] = useState<boolean>(false);
 
   return (
-    <div className="flex flex-col items-center w-full min-h-screen">
+    <div className="flex flex-col items-center w-full h-screen">
       <div className="flex justify-around w-full mt-20 h-9">
         <div
           className={`flex justify-center w-1/2 border-b-2 cursor-pointer ${
@@ -107,25 +147,61 @@ const Page = () => {
         </div>
       </div>
 
-      <div className="flex items-center justify-center w-full h-72">
-        <Image src={settingCharacter} alt="캐릭터" width={150} height={150} />
+      <div
+        className={`relative flex items-center justify-center w-full h-72`}
+        style={{ backgroundImage: `url(${buyBackground})`, backgroundSize: 'auto 100%' }}
+      >
+        <Image src={buyItem} alt="캐릭터" width={150} height={150} />
+        {showBuyButton && (
+          <button className="absolute z-10 w-20 rounded-lg h-9 -bottom-4 bg-main-yellow drop-shadow-lg">
+            구매하기
+          </button>
+        )}
       </div>
 
-      <div className="w-full mt-auto h-[470px] mb-[70px] bg-content-yellow overflow-y-scroll">
+      <div className="w-full mt-auto h-[450px] mb-[70px] bg-content-yellow overflow-y-scroll">
         {/* 아이템 목록 */}
         <div className="grid w-full grid-cols-2 gap-4 px-6 py-6">
           {filteredItems.map((item, index) => (
             <div
               key={index}
-              className="flex flex-col items-center justify-center p-4 shadow-md cursor-pointer bg-main-background rounded-xl hover:bg-bg-yellow"
+              className={clsx(
+                'relative flex flex-col items-center justify-center p-4 shadow-md cursor-pointer rounded-xl bg-main-background',
+                item.id === selectedItemId
+                  ? 'bg-bg-yellow'
+                  : item.isOwned
+                    ? 'bg-gray-200'
+                    : 'hover:bg-bg-yellow'
+              )}
               onClick={() => {
                 if (item.category !== '배경') {
-                  setSettingCharacter(item.appliedImage);
+                  setSelectedItemId(item.id);
+                  setBuyItem(item.appliedImage);
+                  if (!item.isOwned) {
+                    setShowBuyButton(true);
+                  } else {
+                    setShowBuyButton(false);
+                  }
                 } else {
-                  return;
+                  if (item.name == '벚꽃 배경') {
+                    setBuyBackground('/images/background/store_cherryBlossom.png');
+                  } else {
+                    setBuyBackground('/images/background/store_beach.png');
+                  }
+                  setSelectedItemId(item.id);
+                  if (!item.isOwned) {
+                    setShowBuyButton(true);
+                  } else {
+                    setShowBuyButton(false);
+                  }
                 }
               }}
             >
+              {item.isOwned && (
+                <p className="absolute px-2 py-1 text-sm -left-2 -top-2 bg-main-yellow rounded-xl">
+                  보유 중
+                </p>
+              )}
               <Image
                 src={item.image}
                 alt={item.name}
