a9efd214bfeb0f2582c8e28bd098c98f50db7ac9
xuuwon <dumpling1011@gmail.com>
2025-05-30 21:03:53 +0900

    style: Nav바 스타일 수정

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import clsx from 'clsx';
 import { CalendarDays, ChartColumn, House, User } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
@@ -10,7 +11,18 @@ const NavBar = () => {
   const iconStyle = 'hover:text-[#ffad20] cursor-pointer';
 
   return (
-    <div className="fixed bottom-0 left-0 flex items-center justify-between w-full h-[70px] px-8 sm:px-10 md:px-12">
+    <div
+      className={clsx(
+        'fixed bottom-0 left-0 flex items-center justify-between w-full h-[70px] px-8 sm:px-10 md:px-12 bg-main-background'
+      )}
+      style={{
+        maxWidth: '768px',
+        margin: '0 auto',
+        width: '100%',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      }}
+    >
       <House className={iconStyle} onClick={() => router.push('/home')} />
       <CalendarDays className={iconStyle} onClick={() => router.push('/calendar')} />
       <ChartColumn className={iconStyle} onClick={() => router.push('/statistic')} />
