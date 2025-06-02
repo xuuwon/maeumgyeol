ce6312b54e83916c0a292fda35a32a548e293aab
xuuwon <dumpling1011@gmail.com>
2025-06-02 17:44:21 +0900

    feat: iOS PWA 지원을 위한 메타 태그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { usePathname } from 'next/navigation';
 import './globals.css';
 import NavBar from '@/components/navBar/NavBar';
 import Header from '@/components/header/Header';
-import Head from 'next/head';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -26,15 +25,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="ko">
-      <Head>
+      <head>
         {/* ✅ PWA 관련 태그 */}
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <link rel="apple-touch-icon" href="/images/icon.png" />
-      </Head>
+        <meta name="apple-mobile-web-app-title" content="마음결" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <link rel="apple-touch-icon" href="/images/icon-180.png" sizes="180x180" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className="bg-gray-100">
         <div className="w-full max-w-[700px] mx-auto min-h-screen bg-main-background">
           <div className="relative h-full">
