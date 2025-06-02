0097f9b013ac73a56899b13953d82336163ee0cb
xuuwon <dumpling1011@gmail.com>
2025-06-03 01:38:01 +0900

    feat: 랜딩 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,23 +10,24 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-around w-full h-screen px-4 py-12 sm:px-6 md:px-8">
       {/* 제목 */}
-      <div className="text-3xl sm:text-4xl">마음결 (心結)</div>
+      <div className="text-2xl sm:text-3xl">마음결 (心結)</div>
 
       {/* 이미지 */}
       <div className="iphoneSE:w-56">
         <Image
           src="/images/landing.png" // public 폴더 기준 경로 또는 외부 URL
           alt="설명 텍스트"
-          width={250}
-          height={250}
+          width={270}
+          height={270}
         />
       </div>
 
       {/* 서비스 소개 */}
       <div className="w-64 max-w-full sm:w-full">
         <p className="text-center">
-          마음결은 당신의 감정을 이해하고 치유하는 맞춤형 심리 상담 플랫폼입니다.
-          <br /> 언제 어디서나 편안하게, 전문가와 함께 마음 건강을 케어하세요.
+          마음결은 당신의 감정을 이해하고 하루를 편안히 마무리하게 돕는 서비스입니다.💝
+          <br />
+          귀여운 감자몽과 함께 마음의 휴식을 경험해보세요.🥰
         </p>
       </div>
 
