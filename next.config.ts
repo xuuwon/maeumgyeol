de08a2c8f60d5c26d9a0b5738af5350a4cf7cd77
xuuwon <dumpling1011@gmail.com>
2025-06-07 21:31:15 +0900

    feat: 외부 이미지 도메인 remotePatterns 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sentiment-server.duckdns.org',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
