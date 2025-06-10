645cd6e8e4fecc9d58dc3676deed9fa040a54f7d
xuuwon <dumpling1011@gmail.com>
2025-06-10 19:52:24 +0900

    refactor: Lazy Loading을 eager로 변경, 이미지 width/height를 명시함으로써 로딩 지연 현상 해결

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -61,13 +61,14 @@ const Page = () => {
           <SwiperSlide key={idx}>
             <div className="flex flex-col items-center justify-around h-full px-6 py-12 text-center">
               {/* 이미지 */}
-              <div className="w-[250px] h-[400px] relative drop-shadow-lg">
+              <div className="drop-shadow-lg">
                 <Image
                   src={slide.imgSrc}
                   alt={slide.title}
-                  fill
+                  width={250}
+                  height={400}
                   className="object-contain"
-                  priority={idx === 0}
+                  loading="eager"
                 />
               </div>
 
